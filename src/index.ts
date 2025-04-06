512481d8b739503a49bba72af7754345be04dc0f
NightHawkNB <nipunbathiya1256@outlook.com>
2025-04-06 23:28:55 +0530

    [UPDATED]

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,20 @@ import bookingsRouter from "./api/booking";
 import hotelsRouter from "./api/hotel";
 import globalErrorHandlingMiddleware from "./api/middlewares/global-error-handling-middleware";
 
-// Create an Express instance
 const app = express();
 
 app.use(clerkMiddleware());
-// Middleware to parse JSON data in the request body
+
+//* Middleware to parse JSON data in the request body
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://aidf-horizone-frontend-nipunbathiya.netlify.app" }));
 
 connectDB();
-
-// app.use((req, res, next) => {
-//   console.log("Hello World");
-//   next();
-// });
 
 app.use("/api/hotels", hotelsRouter);
 app.use("/api/bookings", bookingsRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
-// Define the port to run the server
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
